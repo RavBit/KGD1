88fbf5925588c9818128ac76e77b96f7b4e0b54b
Sergi van Ravenswaay <svanravenswaay@perspectiveworks.nl>
2017-10-04 13:35:41 +0200

    fist slot machine example

---
+++ PGEXM/Assets/SM_Image.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SM_Image : MonoBehaviour {
+    private Sequence sequence;
+    private Image thisImage;
+
+        // Use this for initialization
+        void Start () {
+            sequence = DOTween.Sequence();
+            thisImage = GetComponent<Image>();
+            //thisImage.transform.DOLocalMoveY(300, 10, false);
+        }
+
+        // Update is called once per frame
+        void Update () {
+            sequence.Kill();
+        thisImage.rectTransform.Translate(Vector3.up * 10);
+        if (thisImage.rectTransform.localPosition.y >= 300)
+            {
+                thisImage.transform.localPosition = new Vector3(0, -300, 0);
+            }
+        }
+    }
--- PGEXM/Assets/Scripts/Manager/Slot_Machine_Manager.cs
+++ PGEXM/Assets/Scripts/Manager/Slot_Machine_Manager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Slot_Machine_Manager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
